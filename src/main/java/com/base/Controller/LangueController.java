0b255cd91fa81fd0088bc047b144376244c2738c
barry <ousmanebarry994@gmail.com>
2018-12-20 09:57:10 +0000

    modification controller langue

--- src/main/java/com/base/Controller/LangueController.java
+++ src/main/java/com/base/Controller/LangueController.java
@@ -1,12 +1,18 @@
 package com.base.Controller;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.base.Repository.LangueRepository;
 import com.base.Entities.Langue;
+import com.base.Entities.Niveauetude;
 
 @CrossOrigin(origins = "http://localhost:4200",allowedHeaders="*")
 @RestController
@@ -15,5 +21,29 @@ public class LangueController {
 	
 	@Autowired
 	LangueRepository langueRepository;
+	
+	
+	@GetMapping("/langue/nom")
+	public List<String> getAllnomEthnies() {
+		try
+		{
+		   System.out.println("Get all Ethnies...");
+		   List<Langue> listeLangue = new ArrayList<>();
+		   langueRepository.findAll().forEach(listeLangue::add);
+		   List<String> nomlangue = new ArrayList<>();
+		   Iterator<Langue> it= listeLangue.iterator();
+		   while(it.hasNext())
+		   {
+			   nomlangue.add(it.next().getNom());
+			   
+			   
+		   }
+		   return nomlangue;
+		}
+		catch(Exception e)
+		{
+			return null;
+		}
+	}
 
 }
