d2a2a7a52ef297688aac260b0ac887172197004f
barry <ousmanebarry994@gmail.com>
2019-01-25 14:39:52 +0000

    vend

--- src/main/java/com/base/Repository/ComptenceRepository.java
+++ src/main/java/com/base/Repository/ComptenceRepository.java
@@ -15,13 +15,11 @@ public interface ComptenceRepository extends CrudRepository<Competence ,Integer>
 	public List<Competence> findByDescription(String description);
 	@Query("SELECT c FROM Competence c where c.description like %:description% ORDER BY c.idcompetence ASC")
 	public List<Competence> findByDescriptionPagination(@Param("description")String description,Pageable pageable);
-<<<<<<< HEAD
+	
 	@Query("Select c from Competence c ")
 	List<Competence> mesCompetence(Pageable pageable);
-	
-=======
+
 	@Query("SELECT c.employees FROM Competence c where c.description in :listeCompetenceDescription")
 	List<Employee>rechercheEmployeeCompetenceDescription(@Param("listeCompetenceDescription") List<String> listeCompetenceDescription,Pageable pageable);
->>>>>>> 1ca43ea6be030601ab85634b98e9120cc4e0a3c8
 
 }
